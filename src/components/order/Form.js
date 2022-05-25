cf9c71bcd5739fd055f48b81e06060f198da3152
Alina <voey.alina@gmail.com>
2022-05-24 21:26:07 -0400

    added images and styling to order form

--- src/components/order/Form.js
+++ src/components/order/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import RangeSlider from 'react-bootstrap-range-slider'
 import Col from 'react-bootstrap/Col'
 
@@ -7,46 +8,52 @@ class OrderForm extends Component {
     return (
       <>
         <form onSubmit={this.props.handleSubmit}>
-          <div className='drink-option'>
-            <label>
-              <input
-                type="radio"
-                name="drinkType"
-                value='Coffee'
-                onChange={this.props.onDrinkOption}
-                checked={this.props.drink.drinkType === 'Coffee'}
-              /> Coffee
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="drinkType"
-                value='Latte'
-                onChange={this.props.onDrinkOption}
-                checked={this.props.drink.drinkType === 'Latte'}
-              /> Latte
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="drinkType"
-                value='Americano'
-                onChange={this.props.onDrinkOption}
-                checked={this.props.drink.drinkType === 'Americano'}
-              /> Americano
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="drinkType"
-                value='Capuccino'
-                onChange={this.props.onDrinkOption}
-                checked={this.props.drink.drinkType === 'Capuccino'}
-              /> Capuccino
-            </label>
+          <div id='drink-section' className='drink-option'>
+            <h4>Drink Selection</h4>
+            <div className='drink-grid'>
+              <label>
+                <input
+                  type="radio"
+                  name="drinkType"
+                  value='Coffee'
+                  onChange={this.props.onDrinkOption}
+                  checked={this.props.drink.drinkType === 'Coffee'}
+                /> <div className='form-coffee-img'><img src={process.env.PUBLIC_URL + '/drink-coffee.jpg'}/></div> Coffee
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="drinkType"
+                  value='Latte'
+                  onChange={this.props.onDrinkOption}
+                  checked={this.props.drink.drinkType === 'Latte'}
+                /> <div className='form-coffee-img'><img src={process.env.PUBLIC_URL + '/drink-latte.jpg'}/></div> Latte
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="drinkType"
+                  value='Americano'
+                  onChange={this.props.onDrinkOption}
+                  checked={this.props.drink.drinkType === 'Americano'}
+                /> <div className='form-coffee-img'><img src={process.env.PUBLIC_URL + '/drink-americano.jpg'}/></div>  Americano
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="drinkType"
+                  value='Capuccino'
+                  onChange={this.props.onDrinkOption}
+                  checked={this.props.drink.drinkType === 'Capuccino'}
+                /> <div className='form-coffee-img'><img src={process.env.PUBLIC_URL + '/drink-capp.jpg'}/></div> Capuccino
+              </label>
+            </div>
+            <div className='arrow-down'><a href='#milk-section'><img src={process.env.PUBLIC_URL + '/arrow-down.png'}/></a></div>
           </div>
 
-          <div className='milk-option'>
+          <div id='milk-section' className='milk-option'>
+            <div className='arrow-up'><a href='#drink-section'><img src={process.env.PUBLIC_URL + '/arrow-up.png'}/></a></div>
+            <h4>Milk Option</h4>
             <label>
               <input
                 type="radio"
@@ -54,7 +61,7 @@ class OrderForm extends Component {
                 value='Whole'
                 onChange={this.props.onDrinkOption}
                 checked={this.props.drink.milk === 'Whole'}
-              /> Whole
+              /> <div className='form-milk-img'><img src={process.env.PUBLIC_URL + '/milk-whole.png'}/></div> Whole
             </label>
             <label>
               <input
@@ -63,7 +70,7 @@ class OrderForm extends Component {
                 value='Oat'
                 onChange={this.props.onDrinkOption}
                 checked={this.props.drink.milk === 'Oat'}
-              /> Oat
+              /> <div className='form-milk-img'><img src={process.env.PUBLIC_URL + '/milk-oat.png'}/></div> Oat
             </label>
             <label>
               <input
@@ -72,7 +79,7 @@ class OrderForm extends Component {
                 value='Almond'
                 onChange={this.props.onDrinkOption}
                 checked={this.props.drink.milk === 'Almond'}
-              /> Almond
+              /> <div className='form-milk-img'><img src={process.env.PUBLIC_URL + '/milk-almond.png'}/></div> Almond
             </label>
             <label>
               <input
@@ -81,28 +88,36 @@ class OrderForm extends Component {
                 value='None'
                 onChange={this.props.onDrinkOption}
                 checked={this.props.drink.milk === 'None'}
-              /> None
+              /> <div className='form-milk-img'><img src={process.env.PUBLIC_URL + '/milk-none.png'}/></div> None
             </label>
+            <div className='arrow-down'><a href='#sugar-section'><img src={process.env.PUBLIC_URL + '/arrow-down.png'}/></a></div>
           </div>
 
-          <div className="sugar-option">
-            <label>Sweetness level</label>
-            <Col xs="6">
-              <RangeSlider
-                value={this.props.drink.sugarLevel}
-                name="sugarLevel"
-                min={0}
-                max={100}
-                step={10}
-                onChange={this.props.onDrinkOption}
-                tooltip="on"
-                tooltipPlacement='bottom'
-                size="sm"
-              />
-            </Col>
+          <div id='sugar-section' className='sugar-option'>
+            <div className='arrow-up'><a href='#milk-section'><img src={process.env.PUBLIC_URL + '/arrow-up.png'}/></a></div>
+            <h4>Sweetness Level</h4>
+            <div className="d-flex align-items-center justify-content-center">
+              <Col className='sugar-slider' xs="6">
+                <RangeSlider
+                  variant='warning'
+                  value={this.props.drink.sugarLevel}
+                  name="sugarLevel"
+                  min={0}
+                  max={100}
+                  step={10}
+                  onChange={this.props.onDrinkOption}
+                  tooltip="on"
+                  tooltipPlacement='bottom'
+                  size="sm"
+                />
+              </Col>
+            </div>
+            <div className='arrow-down'><a href='#size-section'><img src={process.env.PUBLIC_URL + '/arrow-down.png'}/></a></div>
           </div>
 
-          <div className='size-option'>
+          <div id='size-section' className='size-option'>
+            <div className='arrow-up'><a href='#sugar-section'><img src={process.env.PUBLIC_URL + '/arrow-up.png'}/></a></div>
+            <h4>Drink Size</h4>
             <label>
               <input
                 type="radio"
@@ -110,7 +125,7 @@ class OrderForm extends Component {
                 value='Small'
                 onChange={this.props.onDrinkOption}
                 checked={this.props.drink.size === 'Small'}
-              /> Small
+              /> <div className='form-size-s-img'><img src={process.env.PUBLIC_URL + '/drink-size.png'}/></div> Small
             </label>
             <label>
               <input
@@ -119,7 +134,7 @@ class OrderForm extends Component {
                 value='Medium'
                 onChange={this.props.onDrinkOption}
                 checked={this.props.drink.size === 'Medium'}
-              /> Medium
+              /> <div className='form-size-m-img'><img src={process.env.PUBLIC_URL + '/drink-size.png'}/></div>  Medium
             </label>
             <label>
               <input
@@ -128,41 +143,53 @@ class OrderForm extends Component {
                 value='Large'
                 onChange={this.props.onDrinkOption}
                 checked={this.props.drink.size === 'Large'}
-              /> Large
+              /> <div className='form-size-l-img'><img src={process.env.PUBLIC_URL + '/drink-size.png'}/></div>  Large
             </label>
+            <div className='add-drink-btn'>
+              <button onClick={this.props.addDrink} type='click'>Add Drink</button>
+            </div>
           </div>
 
-          <button onClick={this.props.addDrink} type='click'>Add Drink</button>
+          <div id='order-section' className='order-details'>
+            <h4>Order Details</h4>
+            {this.props.order.drinks.map(drink => (
+              <div key={drink._id}>
+                <p>{drink.size} {drink.drinkType}, Milk: {drink.milk}, Sweetness Level: {drink.sugarLevel}%</p>
+              </div>
+            ))}
+            <p>Total Price: ${this.props.order.price}</p>
+            <a href='#drink-section'>Add Another Drink</a>
 
-          <div className="order-name">
-            <label> Enter your name
-              <input
-                type="text"
-                name="name"
-                placeholder='name'
-                value = {this.props.order.name}
-                onChange={this.props.handleChange}
-              />
-            </label>
+            <div className="order-name">
+              <label> Enter your name
+                <input
+                  type="text"
+                  name="name"
+                  placeholder='name'
+                  value = {this.props.order.name}
+                  onChange={this.props.handleChange}
+                />
+              </label>
+            </div>
+
+            <div className="order-email">
+              <label> Enter your email
+                <input
+                  type="text"
+                  name="email"
+                  placeholder='email'
+                  value = {this.props.order.email}
+                  onChange={this.props.handleChange}
+                />
+              </label>
+            </div>
+
+            <button type='Submit'>Place Order</button>
           </div>
-
-          <div className="order-email">
-            <label> Enter your email
-              <input
-                type="text"
-                name="email"
-                placeholder='email'
-                value = {this.props.order.email}
-                onChange={this.props.handleChange}
-              />
-            </label>
-          </div>
-
-          <button type='Submit'>Place Order</button>
         </form>
       </>
     )
   }
 }
 
-export default OrderForm
+export default withRouter(OrderForm)
